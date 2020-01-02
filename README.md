f9eac13d17695f97ab39744b8772d515452a920b
csisl <stncallahan@gmail.com>
2020-01-02 17:18:03 -0600

    added more sentences and terms after a netflix binge / inspiration

--- README.md
+++ README.md
@@ -9,3 +9,12 @@ inspired by [r/itsaunixsystem](https://www.reddit.com/r/itsaunixsystem/)
 ```
 python gen_script.py
 ```
+
+### some of my favorite output 
+
+* Give me a sec! The have interface in their firewall. This will be tricky, but if I can just VPN in their URL we'll be golden!
+* I need to hack into mainframe with the script 
+* I need more time to get access to their admin. They have a URL setup that is making me need to brute force to see their decryption.
+* I got this. I just need to compile and I can see their Java. After that, a simple sudo will do, and I'm in.
+
+Please add more terms and sentence combinations!
--- gen_script.py
+++ gen_script.py
@@ -8,12 +8,13 @@ from random import randint, choice
 def get_prog_noun():
 
 	prog_nouns = [ "IP", "firewall", "blockchain",
-			       "encryption", "decryption", "GUI",
-				   "interface", "root", "database",
-				   "SQL", "Java", "Python", "script",
-				   "admin", "RAM", "mainframe",
-				   "terminal", "hard drive", "SSL",
-				   "port", "URL", "router" ]
+                   "encryption", "decryption", "GUI",
+                   "interface", "root", "database",
+                   "SQL", "Java", "Python", "script",
+                   "admin", "RAM", "mainframe",
+                   "terminal", "hard drive", "SSL",
+                   "port", "URL", "router", "hashcat",
+                   "Linux", "Kali", "HTTPS" ]
 
 	return prog_nouns[randint(0, len(prog_nouns)-1)]
 
@@ -21,11 +22,11 @@ def get_prog_noun():
 def get_prog_verb():
 
 	prog_verbs = [ "hack", "brute force", "scan",
-			       "decrypt", "encrypt", "load",
+				   "decrypt", "encrypt", "load",
 				   "test", "decompile", "compile",
 				   "tunnel", "VPN", "execute",
 				   "sudo", "link", "download",
-				   "upload", "inject" ]
+				   "upload", "inject", "SSH" ]
 
 	return prog_verbs[randint(0, len(prog_verbs)-1)]
 
@@ -41,18 +42,27 @@ def get_garbage_sentence():
 			"I don't know how to %s a %s like this. They must be using %s" ]
 
 	nnvn = [ "Give me a sec! The have %s in their %s. This will be tricky, " \
-			    "but if I can just %s in their %s we'll be golden!",
+				"but if I can just %s in their %s we'll be golden!",
 			"I don't have a %s or %s but if I can just %s then I will have " \
-			    "access to their %s",
+				"access to their %s",
 			"Wow. I've never seen a %s like this. They even used a %s to %s into the %s",
-			"Sure I can get access to their %s. I just need a %s to %s and I'm golden." \
+			"Sure I can get access to their %s. I just need a %s to %s and I'm golden. " \
 				"Go get me %s",
 			"I need more time to get access to their %s. They have a %s setup that is " \
-				"making me need to %s to see their %s" ]
+				"making me need to %s to see their %s",
+			"Dude you're crazy. Unless you know %s and %s to %s into %s, you're going to " \
+				"want to get me a laptop stat.",
+			"I don't know how this is happening! They used %s with %s to %s into our %s!" ]
+	
+	vnv = [ "I need to %s to get access to their %s. Unless you know how to %s",
+			"There's no use! Whoever it was knows how to %s with %s. I can't even %s to save us.",
+			"I got this. I just need to %s and I can see their %s. After that, a simple " \
+				"%s will do, and I'm in." ]
 
 
 	mapping = { vnn[randint(0, len(vnn)-1)] : "vnn",
-			    nnvn[randint(0, len(nnvn)-1)] : "nnvn" }
+				nnvn[randint(0, len(nnvn)-1)] : "nnvn",
+				vnv[randint(0, len(vnv)-1)] : "vnv" }
 
 	return choice(list(mapping.items()))
 
@@ -66,7 +76,9 @@ def main():
 			print(sentence % (get_prog_verb(), get_prog_noun(), get_prog_noun()))
 		elif sen_type == "nnvn":
 			print(sentence % (get_prog_noun(), get_prog_noun(),
-		              		  get_prog_verb(), get_prog_noun()))
+							  get_prog_verb(), get_prog_noun()))
+		elif sen_type == "vnv":
+			print(sentence % (get_prog_verb(), get_prog_noun(), get_prog_verb()))
 		input("Press Enter to continue or ctrl-c to stop")
 		sentence, sen_type = get_garbage_sentence()
 	
